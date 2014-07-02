423f5dfb4668c9c6d61d886e70dccc40d4d96f11
Christoph Gockel <github@christophgockel.de>
2014-07-02 09:56:10 +0100

    Board can return its columns

--- board.rb
+++ board.rb
@@ -1,25 +1,40 @@
 Move = Struct.new(:symbol, :location)
 
 class Board
+  BOARD_SIZE = 3
+  attr_reader :cells
+
   def initialize
     @cells = []
   end
 
   def empty?
-    @cells.empty?
+    cells.empty?
   end
 
   def set(move)
-    @cells[move.location] = move.symbol
+    cells[move.location] = move.symbol
   end
 
   def rows
     rows = []
 
-    @cells.each_slice(3) do |element|
+    cells.each_slice(BOARD_SIZE) do |element|
       rows << element
     end
 
     rows
+  end
+
+  def columns
+    columns = []
+
+    BOARD_SIZE.times do |column|
+      columns << rows.map do |row|
+        row[column]
+      end
+    end
+
+    columns
   end
 end
--- board_spec.rb
+++ board_spec.rb
@@ -4,32 +4,38 @@ require 'board'
 
 
 describe Board do
-  it "is empty when just created" do
+  it 'is empty when just created' do
     board = Board.new
     expect(board.empty?).to be true
   end
 
-  it "is not empty when it got a move placed" do
+  it 'is not empty when it got a move placed' do
     board = Board.new
     board.set(some_move)
 
     expect(board.empty?).to be false
   end
 
-  it "can return its rows" do
-    board = board_with("111222333")
+  it 'can return its rows' do
+    board = board_with('111222333')
     
-    expect(board.rows).to eq [["1", "1", "1"], ["2", "2", "2"], ["3", "3", "3"]]
+    expect(board.rows).to eq [['1', '1', '1'], ['2', '2', '2'], ['3', '3', '3']]
   end
 
+  it 'can return its columns' do
+    board = board_with('123123123')
+    expect(board.columns).to eq [['1', '1', '1'], ['2', '2', '2'], ['3', '3', '3']]
+  end
+
+
   def some_move
-    Move.new("x", 0)
+    Move.new('x', 0)
   end
 
   def board_with(contents)
     board = Board.new
 
-    contents.split("").each_with_index do |symbol, index|
+    contents.split('').each_with_index do |symbol, index|
       board.set(Move.new(symbol, index))
     end
 
