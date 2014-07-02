7d925d5eca0e00e04bb25e50354eb7257513b568
Christoph Gockel <github@christophgockel.de>
2014-07-03 00:12:54 +0100

    Rules can identify winners now

--- rules.rb
+++ rules.rb
@@ -2,28 +2,54 @@ require 'player'
 
 class Rules
   def has_winner?(board)
-     has_winner_in_row?(board) || has_winner_in_column?(board) || has_winner_in_diagonal?(board)
+    has_winner_somewhere?(board) {}
   end
 
-  def has_winner_in_row?(board)
+  def winner(board, &block)
+    the_winner = nil
+    has_winner_somewhere?(board) { |winner| the_winner = winner }
+    the_winner
+  end
+
+  def has_winner_somewhere?(board, &block)
+    has_winner_in_row?(board, &block) ||
+    has_winner_in_column?(board, &block) ||
+    has_winner_in_diagonal?(board, &block)
+  end
+
+  def has_winner_in_row?(board, &block)
     board.rows.any? do |row|
-      line_has_winner?(row)
+      line_has_winner?(row, &block)
     end
   end
 
-  def has_winner_in_column?(board)
+  def has_winner_in_column?(board, &block)
     board.columns.any? do |column|
-      line_has_winner?(column)
+      line_has_winner?(column, &block)
     end
   end
 
-  def has_winner_in_diagonal?(board)
+  def has_winner_in_diagonal?(board, &block)
     board.diagonals.any? do |diagonal|
-      line_has_winner?(diagonal)
+      line_has_winner?(diagonal, &block)
     end
   end
 
-  def line_has_winner?(line)
-    line.all? { |cell| cell == Player::X || cell == Player::O }
+  def line_has_winner?(line, &block)
+    if all_x?(line)
+      block.call(Player::X) if block_given?
+      true
+    elsif all_o?(line)
+      block.call(Player::O) if block_given?
+      true
+    end
+  end
+
+  def all_x?(line)
+    line.all? { |cell| cell == Player::X  }
+  end
+
+  def all_o?(line)
+    line.all? { |cell| cell == Player::O }
   end
 end
--- rules_spec.rb
+++ rules_spec.rb
@@ -1,6 +1,7 @@
 require 'rspec'
 
 require 'rules'
+require 'player'
 require 'spec_helper'
 
 describe Rules do
@@ -25,5 +26,26 @@ describe Rules do
 
   it 'knows when a board has a winning diagonal' do
     expect(rules.has_winner?(board_with('x   x   x'))).to eq true
+  end
+
+  it 'knows the winner' do
+    expect(rules.winner(board_with('xxx      '))).to eq Player::X
+    expect(rules.winner(board_with('ooo      '))).to eq Player::O
+    expect(rules.winner(board_with('   xxx   '))).to eq Player::X
+    expect(rules.winner(board_with('      ooo'))).to eq Player::O
+    expect(rules.winner(board_with('x  x  x  '))).to eq Player::X
+    expect(rules.winner(board_with(' o  o  o '))).to eq Player::O
+    expect(rules.winner(board_with('  x  x  x'))).to eq Player::X
+    expect(rules.winner(board_with('o   o   o'))).to eq Player::O
+    expect(rules.winner(board_with('  x x x  '))).to eq Player::X
+  end
+
+  it 'returns nil when no one is the winner' do
+    expect(rules.winner(board_with('xox      '))).to be_nil
+  end
+
+  it 'knows when there is no winner' do
+    expect(rules.has_winner?(board_with('xox      '))).to eq false
+    expect(rules.has_winner?(board_with('oxoxoxxox'))).to eq false
   end
 end
