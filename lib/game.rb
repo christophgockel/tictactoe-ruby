9fb40842b5f610c544393ee146565ea825231c5f
Christoph Gockel <github@christophgockel.de>
2014-07-17 15:57:21 +0100

    Cleaned up Game and its specs

--- lib/game.rb
+++ lib/game.rb
@@ -3,17 +3,12 @@ require 'board'
 class Game
   attr_reader :players, :board, :display
 
-  def initialize(players, board, display)
-    @players = players
-    @board   = board || Board.new
-    @display = display
-  end
-
-  def self.init(player_a, player_b, board = nil)
-    Game.new([player_a, player_b], board, nil)
-  end
-
   class Over < RuntimeError
+  end
+
+  def initialize(player_a, player_b, board = nil)
+    @players = [player_a, player_b]
+    @board   = board || Board.new
   end
 
   def play_next_round
@@ -29,37 +24,11 @@ class Game
 
   def winner
     return players.first.mark if board.winner?(players.first.mark)
-    return players.last.mark if board.winner?(players.last.mark)
+    return players.last.mark  if board.winner?(players.last.mark)
     ''
   end
 
-
-
-
-  def start
-    begin
-      begin
-        ensure_enough_players
-
-        display_board
-        place_move_of(players.first)
-        switch_players
-      rescue Board::InvalidMove
-        display.display_invalid_move_message
-      end
-    end while game_is_ongoing
-    display_board
-
-    winner_of_game
-  end
-
-  def game_is_ongoing
-    board.is_completed? == false
-  end
-
-  def display_board
-    display.display_board(board)
-  end
+  private
 
   def place_move_of(player)
     move = player.next_move(board)
@@ -68,26 +37,5 @@ class Game
 
   def switch_players
     players.reverse!
-  end
-
-  def winner_of_game
-    if board.winner?(players.first.mark)
-      players.first.mark
-    elsif board.winner?(players.last.mark)
-      players.last.mark
-    else
-      ''
-    end
-  end
-
-  class InsufficientNumberOfPlayers < RuntimeError
-  end
-
-  private
-
-  def ensure_enough_players
-    if players.length != 2
-      raise InsufficientNumberOfPlayers
-    end
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,154 +5,65 @@ require 'board'
 require 'fake_player'
 
 describe Game do
-  let(:player_a) { object_double(FakePlayer.new('a', 5)).as_null_object }
-  let(:player_b) { object_double(FakePlayer.new('b', 4)).as_null_object }
-  let(:board)    { instance_double(Board).as_null_object }
-  let(:display)  { double.as_null_object }
+  let(:player_a) { FakePlayer.new('a', 1) }
+  let(:player_b) { FakePlayer.new('b', 2) }
+  let(:board)    { board_with('         ') }
+  let(:game)     { Game.new(player_a, player_b, board) }
 
-  before(:each) do
-    allow(player_a).to receive(:next_move).and_return(1)
-    allow(player_b).to receive(:next_move).and_return(2)
+  context 'rules' do
+    it 'switches players for each round' do
+      allow(player_a).to receive(:next_move).and_call_original
+      allow(player_b).to receive(:next_move).and_call_original
 
-    @game = Game.new([player_a, player_b], board, display)
-  end
+      board = board_with('         ')
+      game = Game.new(player_a, player_b, board)
 
-  it 'in each round a player will be asked for its next move' do
-    player_a = instance_double(FakePlayer, :mark => 'a', :next_move => 5)
-    game = Game.init(player_a, player_b, board_with('baba bbaa'))
-    game.play_next_round
+      game.play_next_round
+      game.play_next_round
 
-    expect(player_a).to have_received(:next_move)
-  end
-
-  it 'switches players for each round' do
-    player_a = instance_double(FakePlayer, :mark => 'a', :next_move => 1)
-    player_b = instance_double(FakePlayer, :mark => 'b', :next_move => 9)
-
-    game = Game.init(player_a, player_b, board_with('         '))
-    game.play_next_round
-    game.play_next_round
-
-    expect(player_a).to have_received(:next_move)
-    expect(player_b).to have_received(:next_move)
-  end
-
-  it 'can only be played until its over' do
-    player_a = FakePlayer.new('a', 5)
-    player_b = FakePlayer.new('b', 4)
-
-    game = Game.init(player_a, player_b, board_with('aabababba'))
-
-    expect { game.play_next_round }.to raise_error(Game::Over)
-  end
-
-
-  it 'can return the winner of a game' do
-    player_a = FakePlayer.new('a', 5)
-    player_b = FakePlayer.new('b', 4)
-
-    game = Game.init(player_a, player_b, board_with('aaa      '))
-
-    expect(game.is_ongoing?).to eq false
-    expect(game.winner).to eq 'a'
-  end
-
-
-
-
-  context 'basic rules' do
-    it 'runs until done' do
-      prepare_two_round_game
-      @game.start
-      expect(board).to have_received(:is_completed?).exactly(3).times
+      expect(player_a).to have_received(:next_move)
+      expect(player_b).to have_received(:next_move)
     end
 
-    it 'returns the winner when done' do
-      prepare_player_b_as_winner
-      expect(@game.start).to eq(player_b.mark)
-    end
+    it 'can only be played until its over' do
+      game = Game.new(player_a, player_b, board_with('aabababba'))
 
-    def prepare_two_round_game
-      allow(board).to receive(:is_completed?).and_return(false, false, true)
-    end
-
-    def prepare_player_b_as_winner
-      allow(board).to receive(:winner?).and_return(true)
-    end
-  end
-
-  context 'when not enough players' do
-    let(:game) { Game.new([], board, display) }
-
-    it 'can not be started' do
-      expect { game.start }.to raise_error(Game::InsufficientNumberOfPlayers)
-    end
-  end
-
-  context 'player interaction' do
-    it 'passes the board when asking a player for a move' do
-      @game.start
-      expect(player_a).to have_received(:next_move).with(board).at_least(:once)
-    end
-
-    it 'places player moves on board' do
-      expect(board).to receive(:set_move).with(1, player_a.mark)
-      @game.place_move_of(player_a)
-    end
-
-    it 'is asking each player for moves' do
-      prepare_two_round_game
-      @game.start
-
-      expect(player_a).to have_received(:next_move).exactly(1).times
-      expect(player_b).to have_received(:next_move).exactly(1).times
-    end
-
-    def prepare_two_round_game
-      allow(board).to receive(:is_completed?).and_return(false, true)
-    end
-  end
-
-  context 'display' do
-    it 'will be updated for each game round plus at the end of a game' do
-      prepare_two_round_game
-      @game.start
-      expect(display).to have_received(:display_board).exactly(4).times.with(board)
-    end
-
-    it 'shows a message on invalid moves' do
-      board = board_with('   aaa   ')
-      player_a = FakePlayer.new('a')
-      player_b = FakePlayer.new('b')
-
-      game = Game.new([player_a, player_b], board, display)
-
-      game.start
-
-      expect(display).to have_received(:display_invalid_move_message)
-    end
-
-    def prepare_two_round_game
-      allow(board).to receive(:is_completed?).and_return(false, false, true)
+      expect { game.play_next_round }.to raise_error(Game::Over)
     end
   end
 
   context 'end results' do
-    it 'returns the symbol of the winner' do
-      expect(dummy_game_with('aaa      ').start).to eq 'a'
-      expect(dummy_game_with('      bbb').start).to eq 'b'
+    it 'can return the winner of a game' do
+      game = Game.new(player_a, player_b, board_with('aaa      '))
+
+      expect(game.is_ongoing?).to eq false
+      expect(game.winner).to eq 'a'
     end
 
-    it 'returns empty string when there\'s no winner' do
-      expect(dummy_game_with('xoxo xxoo').start).to eq ''
-    end
+    it 'returns empty string when there is no winner' do
+      game = Game.new(player_a, player_b, board_with('ababcbaba'))
 
-    def dummy_game_with(board_content)
-      board = board_with(board_content)
+      expect(game.is_ongoing?).to eq false
+      expect(game.winner).to eq ''
+    end
+  end
+
+  context 'player interaction' do
+    it 'in each round a player will be asked for its next move' do
       player_a = FakePlayer.new('a', 5)
-      player_b = FakePlayer.new('b', 4)
+      expect(player_a).to receive(:next_move).and_call_original
+      game = Game.new(player_a, player_b, board_with('baba bbaa'))
+      game.play_next_round
+    end
 
-      Game.new([player_a, player_b], board, display)
+    it 'passes the board when asking a player for a move' do
+      expect(player_a).to receive(:next_move).with(board).and_call_original
+      game.play_next_round
+    end
+
+    it 'places player moves on board' do
+      expect(board).to receive(:set_move).with(1, player_a.mark)
+      game.play_next_round
     end
   end
 end
