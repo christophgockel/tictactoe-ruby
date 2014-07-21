c8efdacee96d882bf9ec3ea99780490188b246e6
Christoph Gockel <github@christophgockel.de>
2014-07-21 23:27:07 +0100

    Reduced some duplications in specs for Game

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -15,8 +15,7 @@ describe Game do
       allow(player_a).to receive(:next_move).and_call_original
       allow(player_b).to receive(:next_move).and_call_original
 
-      board = board_with('         ')
-      game = Game.new(player_a, player_b, board)
+      game = game_with_board('         ')
 
       game.play_next_round
       game.play_next_round
@@ -26,7 +25,7 @@ describe Game do
     end
 
     it 'can only be played until its over' do
-      game = Game.new(player_a, player_b, board_with('aabababba'))
+      game = game_with_board('aabababba')
 
       expect { game.play_next_round }.to raise_error(Game::Over)
     end
@@ -34,14 +33,14 @@ describe Game do
 
   context 'end results' do
     it 'can return the winner of a game' do
-      game = Game.new(player_a, player_b, board_with('aaa      '))
+      game = game_with_board('aaa      ')
 
       expect(game.is_ongoing?).to eq false
       expect(game.winner).to eq 'a'
     end
 
     it 'returns empty string when there is no winner' do
-      game = Game.new(player_a, player_b, board_with('ababcbaba'))
+      game = game_with_board('ababcbaba')
 
       expect(game.is_ongoing?).to eq false
       expect(game.winner).to eq ''
@@ -52,17 +51,20 @@ describe Game do
     it 'in each round a player will be asked for its next move' do
       player_a = FakePlayer.new('a', 5)
       expect(player_a).to receive(:next_move).and_call_original
+
       game = Game.new(player_a, player_b, board_with('baba bbaa'))
       game.play_next_round
     end
 
     it 'passes the board when asking a player for a move' do
       expect(player_a).to receive(:next_move).with(board).and_call_original
+
       game.play_next_round
     end
 
     it 'places player moves on board' do
       expect(board).to receive(:set_move).with(1, player_a.mark)
+
       game.play_next_round
     end
 
@@ -71,6 +73,7 @@ describe Game do
       expect(player_a).to receive(:next_move).and_call_original
       game = Game.new(player_a, player_b, board_with('baba bbaa'))
       game.play_next_round
+
       expect(game.round_could_be_played).to eq false
     end
 
@@ -79,7 +82,12 @@ describe Game do
       expect(player_a).to receive(:next_move).and_call_original
       game = Game.new(player_a, player_b, board_with('baba bbaa'))
       game.play_next_round
+
       expect(game.round_could_be_played).to eq true
     end
+  end
+
+  def game_with_board(board_state)
+    Game.new(player_a, player_b, board_with(board_state))
   end
 end
